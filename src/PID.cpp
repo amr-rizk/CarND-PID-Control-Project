1ebff2fc64d2367869933608b5b1db31be08f08a
amr-rizk <amr_aref@live.com>
2020-05-13 23:47:42 -0400

    Initial Commit

---
+++ src/PID.cpp
@@ -0,0 +1,47 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+  
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+  
+  i_error = 0.0;
+  
+  is_initialized = false;
+
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+  if (!is_initialized){
+    prev_cte = cte;
+  }
+  p_error = cte;
+  i_error += cte;
+  d_error = cte - prev_cte;
+  
+  prev_cte = cte;
+  
+  is_initialized = true;
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  return -Kp*p_error - Ki*i_error - Kd*d_error;  // TODO: Add your total error calc here!
+}
